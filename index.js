d1f88ce21b02ce793bb70353034f6ecc97343458
Marci Vallario <marcivallario@gmail.com>
2021-11-28 17:41:10 -0500

    commit tree 43d1e54761f2157c42e2c00a5c590e375392ebaf

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.ceil(Math.random());
+
+const num3 = 19;
+const num4 = 5;
+const mod = num3 % num4;
+
+const max = Math.max(4,8,12,16,20);
